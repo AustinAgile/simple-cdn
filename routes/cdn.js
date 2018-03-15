be3c6fd2c5ea38d063b12dfc8ee612db47fcfe9b
Doug Brown <douglas.brown@mattersight.com>
2018-03-15 12:35:49 -0500

    add a means to read a directory, specific to D3js for now

--- routes/cdn.js
+++ routes/cdn.js
@@ -7,15 +7,30 @@ router.get('/:filename*', function(req, res, next) {
 	};
 
 	var path = req.params.filename+req.params[0];
-	res.sendFile(path, options, function(err) {
-		if (err) {
-			console.log(err);
-			res.status(err.status).end();
-		}
-		else {
-			console.log('Sent:', path);
-		}
-	});
+	if (path == "D3js/prc/inputs") {//Change this to check if is dir, and read files recursively.
+		console.log(path);
+		fs.readdir(options.root+path, function(err, files1) {
+			fs.readdir(options.root+path+"/tests", function(err, files2) {
+				var files = [];
+				files2.forEach(function(file) {
+					files.push("tests/"+file);
+				});
+				files1.forEach(function(file) {
+					if (file != "tests") {
+						files.push(file);
+					}
+				});
+				res.send(JSON.stringify(files));
+			});
+		});
+	} else {
+		res.sendFile(path, options, function (err) {
+			if (err) {
+				console.error(err);
+				res.status(err.status).end();
+			}
+		});
+	}
 });
 
 
